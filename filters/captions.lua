cd84f53e09111c0370efca446a71682eb311da1d
EmmaJChristensen <emma14@live.dk>
2026-05-18 14:57:55 +0200

    Opdateret

--- filters/captions.lua
+++ filters/captions.lua
@@ -1,7 +1,5 @@
-
 return {
   FloatRefTarget = function(float)
-    -- quarto.log.output(float)
     if float.type == "Figure" and float.caption_long ~= nil then
       local alphabet = 'abcdefghijklmnopqrstuvwxyz'
       local captures = pandoc.Inlines({})
@@ -9,13 +7,18 @@ return {
         captures:insert(inline)
       end
       for i, foo in ipairs(float.content) do
-        captures:insert(pandoc.Space())
-        captures:insert(pandoc.Str(string.format("(%s):", alphabet:sub(i, i))))
-        captures:insert(pandoc.Space())
-        for i, inline in ipairs(foo.content[2].content[1].content) do
-          captures:insert(inline)
+        -- nil-check: spring over hvis strukturen ikke matcher
+        if foo.content and foo.content[2] and 
+           foo.content[2].content and foo.content[2].content[1] and
+           foo.content[2].content[1].content then
+          captures:insert(pandoc.Space())
+          captures:insert(pandoc.Str(string.format("(%s):", alphabet:sub(i, i))))
+          captures:insert(pandoc.Space())
+          for i, inline in ipairs(foo.content[2].content[1].content) do
+            captures:insert(inline)
+          end
+          foo.content[2].content[1].content = pandoc.Inlines({pandoc.Str("")})
         end
-        foo.content[2].content[1].content = pandoc.Inlines({pandoc.Str("")})
       end
       if float.attributes["layout-ncol"] ~= nil then    
         float.caption_long.content = captures
@@ -24,3 +27,4 @@ return {
     end
   end
 }
+
